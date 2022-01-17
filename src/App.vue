5c22ea145079e48658fe901f6d29411182a821c3
Dao <duongducdao1511@gmail.com>
2022-01-17 16:57:17 +0700

    setting route

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
 <script setup>
+import {onBeforeMount} from 'vue'
+import {useRouter, useRoute} from 'vue-router'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {firebaseApp, firebaseAuth} from "./composable/useFirebase.js"
+const router = useRouter();
+const route = useRoute();
+onBeforeMount(()=>{
+  onAuthStateChanged(firebaseAuth, user => {
+    if(user && (route.path == '/login'|| route.path == '/register')){
+      router.replace('/');
+    }
+  });
+})
 </script>
 
 <template>
