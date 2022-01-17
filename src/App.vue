044656ed6deda2f82ed5621129cc53d2baa4a4a9
Dao <duongducdao1511@gmail.com>
2022-01-17 14:47:06 +0700

    use vue router

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,8 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Task from "./components/Task.vue"
-import {useStore} from "./store/index.js"
-import {ref} from 'vue'
-const newTask = ref('');
-const store = useStore();
-const addTask = ()=>{
-  store.addTask(newTask.value);
-  newTask.value = "";
-}
-const sortItems = [
-  {name:"All"},
-  {name:"Active"},
-  {name:"Completed"},
-];
-const clear = () => {
-  for(let el of store.filteredTasks){
-    if(el.done){
-      console.log('a');
-      store.removeTask(el);
-    }
-  }
-};
-const filterTask = (state,i)=>{
-  store.activeEl = i
-  store.filteredTasks = store.tasks.filter((value)=>{
-    if(state=="Active"){
-      return value.done == false;
-    } 
-    else if(state=="Completed"){
-      return value.done == true;
-    }
-    else if(state=="All"){
-      return value;
-    }
-  })
-}
 </script>
 
 <template>
-    <div :class="[store.isDark ? `dark` : ``]">
-      <div class="h-screen dark:bg-dark-bg overflow-y-scroll">
-        <div class="z-0 h-72 dark:bg-desktop-dark bg-desktop-light bg-cover bg-center">
-          <div class="container z-10 w-11/12 md:w-3/5 lg:w-2/5 h-auto mx-auto pt-16 pb-40 sm:py-16">
-            <div class="flex justify-between">
-              <h1 class="text-3xl font-bold tracking-widest text-very-light-gray">TODO</h1>
-              <i class="bg-icon-moon dark:bg-icon-sun w-[26px] h-[26px] cursor-pointer transition ease-in delay-1000" @click="store.toggleTheme"></i>
-            </div>
-            <div class="w-full h-12 bg-very-light-gray dark:bg-dark-bg-secondary rounded flex items-center gap-4 px-4 mt-12 mb-8">
-              <div class="w-5 h-5 border-2 rounded-full border-gray-200"></div>
-              <input
-                type="text"
-                class="w-full placeholder:text-dark-grayish-blue dark:bg-dark-bg-secondary dark:text-dark-text focus:outline-none caret-bright-blue"
-                placeholder="Add a new task"
-                v-model="newTask"
-                @keypress.enter="addTask"
-                >
-            </div>
-            <div class="rounded-t overflow-hidden shadow-xl">
-              <Task v-for="(task,i) in store.filteredTasks" :key="i" :task="task"/>
-            </div>
-            <div class="w-full h-12 bg-very-light-gray dark:bg-dark-bg-secondary flex justify-between         items-center px-4 rounded-b relative shadow-xl">
-              <p class="text-xs text-dark-grayish-blue dark:text-dark-text-secondary">{{store.counter}} {{store.counter>=2 ? `items` : `item`}} left</p>
-              <div class="flex text-sm absolute bottom-[-5rem] left-0 rounded w-full h-12 justify-center bg-very-light-gray dark:bg-dark-bg-secondary gap-4 shadow-xl sm:static sm:shadow-none sm:w-fit">
-                <button
-                  @click="filterTask(`${item.name}`,i)"
-                  v-for="(item,i) in sortItems" :key="i"
-                  class="dark:text-dark-text-secondary"
-                  :class="[store.activeEl===i ? `active` : ``]"
-                >
-                  {{item.name}}
-                </button>
-              </div>
-              <button class="text-sm dark:text-dark-text-secondary" @click="clear()">Clear Completed</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  <router-view></router-view>
 </template>
 
 <style>
