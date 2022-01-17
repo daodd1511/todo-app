70ec07eca5309ca1941cb383c88ff4acb66250fd
Dao <duongducdao1511@gmail.com>
2022-01-17 16:58:42 +0700

    add register logic

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,40 @@
 <template>
-    <div class="register">
-        register
+    <div class="login">
+        <h1>Register</h1>
+        <form @submit.prevent="Register">
+            <input type="email" placeholder="Email" v-model="username">
+            <input type="password" placeholder="Password" v-model="password">
+            <input type="submit" value="Register">
+            <p><router-link to="/login">Login</router-link></p>
+        </form>
     </div>
 </template>
+ 
+<script setup>
+import {ref} from 'vue'
+import {useRouter} from 'vue-router'
+import useAuth from "../composable/useAuth.js"
 
-<script>
-    export default {
-        
+const { isAuthenticated, register } = useAuth();
+
+const username = ref("");
+const password = ref("");
+
+const router = useRouter();
+
+const Register = async ()=>{
+    await register(username.value, password.value);
+    gotoHome();
+}
+const gotoHome = ()=>{
+    if(isAuthenticated.value){
+        router.push('/');
     }
+    else{
+        console.log('error');
+    }
+}
+
 </script>
 
 <style scoped>
