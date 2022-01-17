c979a7c7dd3392d5b6b59641eae558fb077b44c6
Dao <duongducdao1511@gmail.com>
2022-01-17 16:58:33 +0700

    add login logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,42 @@
 <template>
     <div class="login">
-        LOGIN
+        <h1>Login</h1>
+        <form @submit.prevent="Login">
+            <input type="email" placeholder="Email" v-model="username">
+            <input type="password" placeholder="Password" v-model="password">
+            <input type="submit" value="Login">
+            <p><router-link to="/register">Register</router-link></p>
+        </form>
+        <button @click="test">test</button>
     </div>
 </template>
 
-<script>
-    export default {
-        
+<script setup>
+import {ref} from 'vue'
+import {useRouter} from 'vue-router'
+import useAuth from "../composable/useAuth.js"
+
+const { isAuthenticated, login } = useAuth();
+
+const username = ref("");
+const password = ref("");
+
+const router = useRouter();
+
+
+const Login = async ()=>{
+    await login(username.value, password.value);
+    gotoHome();
+}
+const gotoHome = ()=>{
+    if(isAuthenticated.value){
+        router.push('/');
     }
+    else{
+        console.log('error');
+    }
+}
+
 </script>
 
 <style scoped>
