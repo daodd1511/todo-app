78cb8c628f2a1ada5edcde20ea3efaf038cf731c
Dao <duongducdao1511@gmail.com>
2022-01-17 15:24:48 +0700

    use firebase

---
+++ src/composable/useFirebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"
+import {getAuth} from "firebase/auth"
+const firebaseConfig = {
+    apiKey: "AIzaSyD5G1tB0tO3pSHEDtY8yzROY9uQrsYFSs4",
+    authDomain: "todo-e47c0.firebaseapp.com",
+    projectId: "todo-e47c0",
+    storageBucket: "todo-e47c0.appspot.com",
+    messagingSenderId: "391559150737",
+    appId: "1:391559150737:web:2231f001dd5f4ec3991330"
+};
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
