5c9b0059caad7ee51df8bb60d55e782c498cd525
Dao <duongducdao1511@gmail.com>
2022-01-26 19:56:17 +0700

    remove router section

--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { createRouter, createWebHistory } from "vue-router";
 import "./index.css";
 import { createPinia } from "pinia";
-import Home from "./views/Home.vue";
-import Login from "./views/Login.vue";
-import Register from "./views/Register.vue";
+import router from "./router/router.js";
 
 const app = createApp(App);
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/", name: "Home", component: Home },
-    { path: "/login", name: "Login", component: Login },
-    { path: "/register", name: "Register", component: Register },
-  ],
-});
 
 app.use(createPinia());
 app.use(router).mount("#app");
