915bc93d287d4d970906fa99e6d5d0d5f354e7d8
Dao <duongducdao1511@gmail.com>
2022-01-27 16:57:21 +0700

    style  register page and add error handling

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,12 +1,34 @@
 <template>
-  <div class="login">
-    <h1>Register</h1>
-    <form @submit.prevent="Register">
-      <input type="email" placeholder="Email" v-model="username" />
-      <input type="password" placeholder="Password" v-model="password" />
-      <input type="submit" value="Register" />
-      <p><router-link to="/login">Login</router-link></p>
-    </form>
+  <div class="check h-screen flex justify-center items-center">
+    <div class="container w-1/3 h-1/2 bg-very-light-gray rounded-md p-4">
+      <h1 class="text-3xl text-center">Register</h1>
+      <form @submit.prevent="Register" class="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          v-model="username"
+          class="w-full h-10"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="password"
+          class="w-full h-10"
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          v-model="passwordConfirm"
+          class="w-full h-10"
+        />
+        <input
+          type="submit"
+          value="Register"
+          class="w-full h-10 bg-dark-bg-secondary text-very-light-grayish-blue rounded-md"
+        />
+        <p><router-link to="/login">Login</router-link></p>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -14,24 +36,53 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import useAuth from "../composable/useAuth.js";
-
-const { isAuthenticated, register } = useAuth();
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../composable/useFirebase.js";
+const { register } = useAuth();
 
 const username = ref("");
 const password = ref("");
+const passwordConfirm = ref("");
 
 const router = useRouter();
 
+const handleError = (error) => {
+  switch (error) {
+    case "auth/email-already-in-use":
+      username.value = "";
+      window.alert("Email already in use! Please try again");
+      break;
+    case "auth/invalid-email":
+      username.value = "";
+      window.alert("Invalid email! Please try again");
+      break;
+    case "auth/weak-password":
+      window.alert("Weak password! Please try again");
+      break;
+  }
+  password.value = "";
+  passwordConfirm.value = "";
+};
 const Register = async () => {
-  await register(username.value, password.value);
-  gotoHome();
+  if (password.value == passwordConfirm.value) {
+    await register(username.value, password.value)
+      .then()
+      .catch((error) => {
+        handleError(error.code);
+      });
+    gotoHome();
+  } else {
+    window.alert("Wrong password confirmation, please try again!");
+    password.value = "";
+    passwordConfirm.value = "";
+  }
 };
 const gotoHome = () => {
-  if (isAuthenticated.value) {
-    router.push("/");
-  } else {
-    console.log("error");
-  }
+  onAuthStateChanged(firebaseAuth, (user) => {
+    if (user) {
+      router.replace("/");
+    }
+  });
 };
 </script>
 
