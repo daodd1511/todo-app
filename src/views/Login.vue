08edc362474fa5c7ae4fbe2be54d67e7b08778df
Dao <duongducdao1511@gmail.com>
2022-01-27 16:57:04 +0700

    style login page and add error handling

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="login">
-    <h1>Login</h1>
-    <form @submit.prevent="Login">
-      <input type="email" placeholder="Email" v-model="username" />
-      <input type="password" placeholder="Password" v-model="password" />
-      <input type="submit" value="Login" />
-      <p><router-link to="/register">Register</router-link></p>
-    </form>
+  <div class="check h-screen flex justify-center items-center">
+    <div class="container w-1/3 h-2/5 bg-very-light-gray rounded-md p-4">
+      <h1 class="text-3xl text-center">Login</h1>
+      <form @submit.prevent="Login" class="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          v-model="username"
+          class="w-full h-10"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="password"
+          class="w-full h-10"
+        />
+        <input
+          type="submit"
+          value="Login"
+          class="w-full h-10 bg-dark-bg-secondary text-very-light-grayish-blue rounded-md"
+        />
+        <p><router-link to="/register">Register</router-link></p>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -14,24 +30,39 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import useAuth from "../composable/useAuth.js";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../composable/useFirebase.js";
 
-const { isAuthenticated, login } = useAuth();
+const { login } = useAuth();
 
 const username = ref("");
 const password = ref("");
 
 const router = useRouter();
 
+const handleError = (error) => {
+  switch (error) {
+    case "auth/wrong-password":
+      window.alert("Wrong password! Please try again");
+      break;
+    case "auth/user-not-found":
+      window.alert("User not found! Please register or try again");
+  }
+};
 const Login = async () => {
-  await login(username.value, password.value);
+  await login(username.value, password.value)
+    .then()
+    .catch((error) => {
+      handleError(error.code);
+    });
   gotoHome();
 };
 const gotoHome = () => {
-  if (isAuthenticated.value) {
-    router.push("/");
-  } else {
-    console.log("error");
-  }
+  onAuthStateChanged(firebaseAuth, (user) => {
+    if (user) {
+      router.push("/");
+    }
+  });
 };
 </script>
 
