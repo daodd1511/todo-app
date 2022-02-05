8fb82a96b787cea0e54c4b368e65e37da443f08a
Dao <duongducdao1511@gmail.com>
2022-02-06 01:54:22 +0700

    create modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="modal absolute top-0 left-0 bottom-0 right-0">
+    <div
+      class="modal-overlay w-full h-full bg-black/40 flex justify-center items-center"
+    >
+      <div
+        class="modal-body w-10/12 h-1/3 sm:w-3/5 md:w-2/5 bg-white dark:bg-dark-bg-secondary rounded-lg flex justify-around items-center flex-col p-5"
+      >
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
