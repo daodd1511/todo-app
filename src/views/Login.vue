35133bd35e152803b9880f4d8601375c540410b9
Dao <duongducdao1511@gmail.com>
2022-02-06 16:16:21 +0700

    add reset password

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -22,13 +22,46 @@
           value="Login"
           class="w-full h-10 bg-dark-bg-secondary text-very-light-grayish-blue rounded-md cursor-pointer"
         />
-        <p><router-link to="/register">Register</router-link></p>
       </form>
+      <div class="pt-6">
+        <p class="float-left">
+          <router-link to="/register">Register</router-link>
+        </p>
+        <button @click="modalResetPassword = true" class="float-right">
+          Reset Password
+        </button>
+      </div>
     </div>
   </div>
   <Modal v-if="modalActive">
     <h1 class="title">{{ errorMsg }}</h1>
     <button @click="modalActive = false" class="btn shadow-red-400/50">
+      Close
+    </button>
+  </Modal>
+  <Modal v-if="modalResetPassword">
+    <input
+      type="email"
+      v-model="resetEmail"
+      placeholder="Your email"
+      class="w-4/5 h-10 placeholder:italic focus:outline-none border-2 border-black rounded-md px-2"
+    />
+    <button
+      @click="ResetPassword()"
+      class="btn w-3/4 lg:w-1/2 h-10 bg-sky-400 shadow-sky-400/50"
+    >
+      Send reset link!
+    </button>
+    <button
+      @click="modalResetPassword = false"
+      class="btn w-3/4 lg:w-1/2 h-10 shadow-red-400/50"
+    >
+      Close
+    </button>
+  </Modal>
+  <Modal v-if="resetPasswordSuccess">
+    <h1 class="title">Please check your email!</h1>
+    <button @click="resetPasswordSuccess = false" class="btn shadow-red-400/50">
       Close
     </button>
   </Modal>
@@ -42,12 +75,17 @@ import useAuth from "../composable/useAuth.js";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../composable/useFirebase.js";
 
-const { login } = useAuth();
+const { login, resetPassword } = useAuth();
 
 const username = ref("");
 const password = ref("");
 const errorMsg = ref("");
 const modalActive = ref(false);
+// Reset password
+const modalResetPassword = ref(false);
+const resetPasswordSuccess = ref(false);
+const resetEmail = ref("");
+
 const router = useRouter();
 
 const handleError = (error) => {
@@ -69,6 +107,13 @@ const Login = async () => {
       handleError(error.code);
     });
   gotoHome();
+};
+const ResetPassword = async () => {
+  if (resetEmail.value != "") {
+    await resetPassword(resetEmail.value);
+    modalResetPassword.value = false;
+    resetPasswordSuccess.value = true;
+  }
 };
 const gotoHome = () => {
   onAuthStateChanged(firebaseAuth, (user) => {
