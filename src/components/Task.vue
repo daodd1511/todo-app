aa9fd39e9580aac067f4f91f656b9e0eccb40362
Dao <duongducdao1511@gmail.com>
2022-04-28 19:41:00 +0700

    Update filter task tab when check or uncheck task

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative flex h-auto w-full items-center gap-4 border-b border-light-grayish-blue bg-very-light-gray px-4 py-[10px] dark:border-dark-border dark:bg-dark-bg-secondary"
+    class="relative flex h-auto w-full items-center gap-2 border-b border-light-grayish-blue bg-very-light-gray px-4 py-[10px] dark:border-dark-border dark:bg-dark-bg-secondary"
   >
     <div
       class="flex h-5 w-5 cursor-pointer items-center justify-center rounded-full"
@@ -9,14 +9,15 @@
         store.toggleTask(
           props.task,
           firebaseAuth.currentUser != null ? firebaseAuth.currentUser.uid : ``
-        )
+        ),
+          RerenderFilteredTasks()
       "
     >
       <i :class="[props.task.done ? taskDone.icon : taskNotDone.icon]"></i>
     </div>
     <label
       :class="[props.task.done ? taskDone.text : taskNotDone.text]"
-      class="w-3/4 break-words"
+      class="w-3/4 break-words px-3"
       v-if="props.task != editedTask"
       @dblclick="edit(props.task)"
     >
@@ -26,7 +27,7 @@
       v-else
       type="text"
       v-model="props.task.content"
-      class="w-3/4"
+      class="w-3/4 px-3"
       @vnode-mounted="({ el }) => el.focus()"
       @blur="doneEdit(props.task)"
       @keyup.enter="doneEdit(props.task)"
@@ -54,6 +55,15 @@ const store = useStore();
 const props = defineProps({
   task: Object,
 });
+const RerenderFilteredTasks = () => {
+  if (store.activeEl == 1) {
+    store.filterTask("Active");
+  }
+  if (store.activeEl == 2) {
+    store.filterTask("Completed");
+  }
+};
+// Css for different task state
 const taskDone = {
   icon: ["bg-icon-check w-[11px] h-[9px]"],
   text: ["line-through text-light-grayish-blue dark:text-dark-text-secondary"],
